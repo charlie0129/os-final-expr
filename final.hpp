0e2e04ab5b90ecbf5be37bd005b0af462ac9a8ab
Charlie Chiang <charlie_c_0129@outlook.com>
2020-11-23 23:53:31 +0800

    feat: Add color codes

--- final.hpp
+++ final.hpp
@@ -1,2 +1,9 @@
 #pragma once
+#include <fstream>
 #include "item.hpp"
+
+#define BLUE_BOLD_TEXT "\033[1;34m"
+#define RED_BOLD_TEXT "\033[1;31m"
+#define GREEN_BOLD_TEXT "\033[1;32m"
+#define REMOVE_TEXT_ATTR "\033[0m"
+#define BLUE_LEADING_ARROW "\033[1;34m=> \033[0m"
