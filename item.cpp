6d02dd9756132cdf5ae71efe64961372ae2dbebf
Charlie Chiang <charlie_c_0129@outlook.com>
2020-11-23 17:44:42 +0800

    fix: Improve mutex locks

--- item.cpp
+++ item.cpp
@@ -6,89 +6,114 @@ Item::Item()
 
 int Item::addItem(std::string name)
 {
-    if (itemList.find(name) != itemList.end())
-        return -1;
     mtx_itemList.lock();
+    if (itemList.find(name) != itemList.end())
+        goto err;
     itemList.insert({name, std::map<std::string, int>{}});
     mtx_itemList.unlock();
     return 0;
+err:
+    mtx_itemList.unlock();
+    return -1;
 }
 
 int Item::addProperty(std::string itemName, std::string propertyName)
 {
+    mtx_itemList.lock();
     std::map<std::string, std::map<std::string, int>>::iterator it;
     if ((it = itemList.find(itemName)) == itemList.end())
-        return -1;
+        goto err;
     if (it->second.find(propertyName) != it->second.end())
-        return -1;
-    mtx_itemList.lock();
+        goto err;
     it->second.insert({propertyName, 0});
     mtx_itemList.unlock();
     return 0;
+err:
+    mtx_itemList.unlock();
+    return -1;
 }
 
 int Item::changePropertyValue(std::string itemName, std::string propertyName, int value)
 {
+    mtx_itemList.lock();
     std::map<std::string, std::map<std::string, int>>::iterator it_itemList;
     std::map<std::string, int>::iterator it_propertyList;
     if ((it_itemList = itemList.find(itemName)) == itemList.end())
-        return -1;
+        goto err;
     if ((it_propertyList = it_itemList->second.find(propertyName)) == it_itemList->second.end())
-        return -1;
-    mtx_itemList.lock();
+        goto err;
     it_propertyList->second = value;
     mtx_itemList.unlock();
     return 0;
+err:
+    mtx_itemList.unlock();
+    return -1;
 }
 
-int Item::getProperty(std::string itemName, std::string propertyName)
+int Item::getPropertyValue(std::string itemName, std::string propertyName)
 {
+    mtx_itemList.lock();
     std::map<std::string, std::map<std::string, int>>::iterator it_itemList;
     std::map<std::string, int>::iterator it_propertyList;
     if ((it_itemList = itemList.find(itemName)) == itemList.end())
-        return -1;
+        goto err;
     if ((it_propertyList = it_itemList->second.find(propertyName)) == it_itemList->second.end())
-        return -1;
+        goto err;
+    mtx_itemList.unlock();
     return it_propertyList->second;
+err:
+    mtx_itemList.unlock();
+    return -1;
 }
 
 int Item::increaseQuantity(std::string itemName, int offset)
 {
+    mtx_itemList.lock();
     std::map<std::string, std::map<std::string, int>>::iterator it_itemList;
     std::map<std::string, int>::iterator it_propertyList;
     if ((it_itemList = itemList.find(itemName)) == itemList.end())
-        return -1;
+        goto err;
     if ((it_propertyList = it_itemList->second.find("quantity")) == it_itemList->second.end())
-        return -1;
-    mtx_itemList.lock();
+        goto err;
     it_propertyList->second += offset;
     mtx_itemList.unlock();
     return 0;
+err:
+    mtx_itemList.unlock();
+    return -1;
 }
 
 int Item::decreaseQuantity(std::string itemName, int offset)
 {
+    mtx_itemList.lock();
     std::map<std::string, std::map<std::string, int>>::iterator it_itemList;
     std::map<std::string, int>::iterator it_propertyList;
     if ((it_itemList = itemList.find(itemName)) == itemList.end())
-        return -1;
+        goto err;
     if ((it_propertyList = it_itemList->second.find("quantity")) == it_itemList->second.end())
-        return -1;
-    mtx_itemList.lock();
+        goto err;
     it_propertyList->second -= offset;
     mtx_itemList.unlock();
     return 0;
+err:
+    mtx_itemList.unlock();
+    return -1;
 }
 
 int Item::getQuantity(std::string itemName)
 {
+    mtx_itemList.lock();
     std::map<std::string, std::map<std::string, int>>::iterator it_itemList;
     std::map<std::string, int>::iterator it_propertyList;
     if ((it_itemList = itemList.find(itemName)) == itemList.end())
-        return -1;
+        goto err;
     if ((it_propertyList = it_itemList->second.find("quantity")) == it_itemList->second.end())
-        return -1;
+        goto err;
+    mtx_itemList.unlock();
     return it_propertyList->second;
+err:
+    mtx_itemList.unlock();
+    return -1;
 }
 
 Item::~Item()
