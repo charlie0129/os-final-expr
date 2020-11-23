8fb85e241571e744b180c988df91412e84cabe39
Charlie Chiang <charlie_c_0129@outlook.com>
2020-11-23 18:26:57 +0800

    feat: Add exception throwing statements refactor: Improve function calls

--- item.cpp
+++ item.cpp
@@ -7,12 +7,15 @@ int Item::addItem(std::string name)
 {
     mtx_itemList.lock();
     if (itemList.find(name) != itemList.end())
+    {
+        mtx_itemList.unlock();
+        throw std::runtime_error{"The item you are trying to add already exists."};
         goto err;
+    }
     itemList.insert({name, std::map<std::string, int>{}});
     mtx_itemList.unlock();
     return 0;
 err:
-    mtx_itemList.unlock();
     return -1;
 }
 
@@ -21,14 +24,21 @@ int Item::addProperty(std::string itemName, std::string propertyName)
     mtx_itemList.lock();
     std::map<std::string, std::map<std::string, int>>::iterator it;
     if ((it = itemList.find(itemName)) == itemList.end())
+    {
+        mtx_itemList.unlock();
+        throw std::runtime_error{"The item you referred to does not exist."};
         goto err;
+    }
     if (it->second.find(propertyName) != it->second.end())
+    {
+        mtx_itemList.unlock();
+        throw std::runtime_error{"The property you are trying to add already exists."};
         goto err;
+    }
     it->second.insert({propertyName, 0});
     mtx_itemList.unlock();
     return 0;
 err:
-    mtx_itemList.unlock();
     return -1;
 }
 
@@ -38,14 +48,21 @@ int Item::changePropertyValue(std::string itemName, std::string propertyName, in
     std::map<std::string, std::map<std::string, int>>::iterator it_itemList;
     std::map<std::string, int>::iterator it_propertyList;
     if ((it_itemList = itemList.find(itemName)) == itemList.end())
+    {
+        mtx_itemList.unlock();
+        throw std::runtime_error{"The item you referred to does not exist."};
         goto err;
+    }
     if ((it_propertyList = it_itemList->second.find(propertyName)) == it_itemList->second.end())
+    {
+        mtx_itemList.unlock();
+        throw std::runtime_error{"The property you referred to does not exist."};
         goto err;
+    }
     it_propertyList->second = value;
     mtx_itemList.unlock();
     return 0;
 err:
-    mtx_itemList.unlock();
     return -1;
 }
 
@@ -55,64 +72,38 @@ int Item::getPropertyValue(std::string itemName, std::string propertyName)
     std::map<std::string, std::map<std::string, int>>::iterator it_itemList;
     std::map<std::string, int>::iterator it_propertyList;
     if ((it_itemList = itemList.find(itemName)) == itemList.end())
+    {
+        mtx_itemList.unlock();
+        throw std::runtime_error{"The item you referred to does not exist."};
         goto err;
+    }
     if ((it_propertyList = it_itemList->second.find(propertyName)) == it_itemList->second.end())
+    {
+        mtx_itemList.unlock();
+        throw std::runtime_error{"The property you referred to does not exist."};
         goto err;
+    }
     mtx_itemList.unlock();
     return it_propertyList->second;
 err:
-    mtx_itemList.unlock();
     return -1;
 }
 
 int Item::increaseQuantity(std::string itemName, int offset)
 {
-    mtx_itemList.lock();
-    std::map<std::string, std::map<std::string, int>>::iterator it_itemList;
-    std::map<std::string, int>::iterator it_propertyList;
-    if ((it_itemList = itemList.find(itemName)) == itemList.end())
-        goto err;
-    if ((it_propertyList = it_itemList->second.find("quantity")) == it_itemList->second.end())
-        goto err;
-    it_propertyList->second += offset;
-    mtx_itemList.unlock();
-    return 0;
-err:
-    mtx_itemList.unlock();
-    return -1;
+    return changePropertyValue(itemName,
+                               "quantity",
+                               getPropertyValue(itemName, "quantity") + offset);
 }
 
 int Item::decreaseQuantity(std::string itemName, int offset)
 {
-    mtx_itemList.lock();
-    std::map<std::string, std::map<std::string, int>>::iterator it_itemList;
-    std::map<std::string, int>::iterator it_propertyList;
-    if ((it_itemList = itemList.find(itemName)) == itemList.end())
-        goto err;
-    if ((it_propertyList = it_itemList->second.find("quantity")) == it_itemList->second.end())
-        goto err;
-    it_propertyList->second -= offset;
-    mtx_itemList.unlock();
-    return 0;
-err:
-    mtx_itemList.unlock();
-    return -1;
+    return increaseQuantity(itemName, offset);
 }
 
 int Item::getQuantity(std::string itemName)
 {
-    mtx_itemList.lock();
-    std::map<std::string, std::map<std::string, int>>::iterator it_itemList;
-    std::map<std::string, int>::iterator it_propertyList;
-    if ((it_itemList = itemList.find(itemName)) == itemList.end())
-        goto err;
-    if ((it_propertyList = it_itemList->second.find("quantity")) == it_itemList->second.end())
-        goto err;
-    mtx_itemList.unlock();
-    return it_propertyList->second;
-err:
-    mtx_itemList.unlock();
-    return -1;
+    return getPropertyValue(itemName, "quantity");
 }
 
 Item::~Item()
--- item.hpp
+++ item.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <iostream>
 #include <mutex>
+#include <stdexcept>
 
 
 class Item
@@ -19,7 +20,7 @@ class Item
         int changePropertyValue(std::string itemName, std::string propertyName, int value);
         int getPropertyValue(std::string itemName, std::string propertyName);
         int increaseQuantity(std::string itemName, int offset = 1);
-        int decreaseQuantity(std::string itemName, int offset = 1);
+        int decreaseQuantity(std::string itemName, int offset = -1);
         int getQuantity(std::string itemName);
 
         ~Item();
