961f76a417efe3ff074b093c514f6900aa895351
Charlie Chiang <charlie_c_0129@outlook.com>
2020-11-23 17:30:23 +0800

    test: Add test code

--- final.cpp
+++ final.cpp
@@ -3,8 +3,19 @@
 
 int main(int argc, char **argv)
 {
+    // -------- Code for Testing Purposes ---------
     Item i{};
-    i.test();
-    i.print();
+    i.addItem("milk");
+    i.addProperty("milk", "quantity");
+    i.changePropertyValue("milk", "quantity", 10);
+    i.decreaseQuantity("milk");
+
+    i.addItem("pancake");
+    i.addProperty("pancake", "quantity");
+    i.changePropertyValue("pancake", "quantity", 10);
+    i.increaseQuantity("pancake");
+
+    std::cout << i.getQuantity("milk") << " " << i.getQuantity("pancake") << std::endl;
+    // -------- Code for Testing Purposes ---------
     return 0;
 }
