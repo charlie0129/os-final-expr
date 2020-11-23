9bad3290b1cacedcd7c0e42f344378569e0240d4
Charlie Chiang <charlie_c_0129@outlook.com>
2020-11-23 17:44:22 +0800

    refactor: Change function name "getProperty" to "getPropertyValue"

--- item.hpp
+++ item.hpp
@@ -20,7 +20,7 @@ class Item
         int addItem(std::string name);
         int addProperty(std::string itemName, std::string propertyName);
         int changePropertyValue(std::string itemName, std::string propertyName, int value);
-        int getProperty(std::string itemName, std::string propertyName);
+        int getPropertyValue(std::string itemName, std::string propertyName);
         int increaseQuantity(std::string itemName, int offset = 1);
         int decreaseQuantity(std::string itemName, int offset = 1);
         int getQuantity(std::string itemName);
