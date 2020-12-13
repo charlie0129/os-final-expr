3e6a98bdb30d6ef27d9c692f03951697bb5beeaa
Golden <d10sama@bupt.edu.cn>
2020-12-13 15:22:52 +0800

    refactor: changed customer

--- customer.cpp
+++ customer.cpp
@@ -1,59 +1,42 @@
 #include "customer.hpp"
 
-extern Item itemRepository;
-extern std::mutex ReSupply;//通知补获锁
-extern std::mutex Check;//顾客结账锁
-
-class Customer
+Customer::Customer(Item& item,std::vector<Supplier*>& spp,Checker* a)
 {
-    Item* i;
-    std::pair<std::string,int> itemname;
-    int *stuff;
-    Supplier* mySpp;
-    int goodNum=rand()%10;
-    std::thread * customerThread = nullptr;
-    Checker* myChecker;
-    public:
-    Customer(Item& item,std::vector<Supplier*>& spp,Checker* a)
+    this->i=&item;
+    srand(time(NULL));
+    int itemIdx = rand() % i->getItemNameList().size();
+    this->itemname={i->getItemNameList()[itemIdx],goodNum};//获取商品名
+    myChecker=a;
+    for(auto j:spp)
     {
-        this->i=&item;
-        srand(time(NULL));
-        int itemIdx = rand() % i->getItemNameList().size();
-        this->itemname={i->getItemNameList()[itemIdx],goodNum};//获取商品名
-        myChecker=a;
-        for(auto j:spp)
+        if(j->getItem_name()==this->itemname.first)
         {
-            if(j->getItem_name()==this->itemname.first)
-            {
-                this->mySpp=j;
-            }
+            this->mySpp=j;
         }
-        customerThread = new std::thread{&Customer::IN, this};
     }
+    customerThread = new std::thread{&Customer::IN, this};
+}
 
-    ~Customer()
+Customer::~Customer()
+{
+    customerThread->join();
+    delete customerThread;
+}
+
+void Customer::IN()
+{
+    int count=0;//从第一个商品开始购买
+    int PresentGoodValue;//检查当前商品数量
+    std::unique_lock<std::mutex> lock(i->getMutex());
+    PresentGoodValue=i->getQuantity(this->itemname.first);
+    while(PresentGoodValue<this->goodNum)
     {
-        customerThread->join();
-        delete customerThread;
+        mySpp->setNeedSupply(true);
+        this->i->getConditionalVariable(itemname.first)->wait(lock);
+        
     }
-
-    
-    void IN()
-    {
-        int count=0;//从第一个商品开始购买
-        int PresentGoodValue;//检查当前商品数量
-        std::unique_lock<std::mutex> lock(i->getMutex());
-        PresentGoodValue=i->getQuantity(this->itemname.first);
-        while(PresentGoodValue<this->goodNum)
-        {
-            mySpp->setNeedSupply(true);
-            this->i->getConditionalVariable(itemname.first)->wait(lock);
-            
-        }
-        i->decreaseQuantity(this->itemname.first,-(this->goodNum));//拿走商品
-        lock.unlock();
-        //进结账队列
-        myChecker->addWaitingLine(this->itemname);
-    }
-
-};
+    i->decreaseQuantity(this->itemname.first,-(this->goodNum));//拿走商品
+    lock.unlock();
+    //进结账队列
+    myChecker->addWaitingLine(this->itemname);
+}
--- customer.hpp
+++ customer.hpp
@@ -1,7 +1,21 @@
 #pragma once
-#include"final.hpp"
 #include <thread>
 #include "item.hpp"
 #include<cmath>
 #include <ctime>
 #include"checker.hpp"
+
+class Customer
+{
+    Item* i;
+    std::pair<std::string,int> itemname;
+    int *stuff;
+    Supplier* mySpp;
+    int goodNum=rand()%10;
+    std::thread * customerThread = nullptr;
+    Checker* myChecker;
+    public:
+    Customer(Item& item,std::vector<Supplier*>& spp,Checker* a);
+    ~Customer();
+    void IN();
+};
